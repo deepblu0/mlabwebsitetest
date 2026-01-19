78059615844f2616e5a7fc4185973d4969b99bae
deepblu0 <berkay.yilmaz.edu@gmail.com>
2026-01-19 23:42:34 +0300

    fix(horizon): restore 26 new calls from history + 741 all calls

--- horizon-data.js
+++ horizon-data.js
@@ -2,12 +2,212 @@ const horizonData = {
   "lastUpdated": "19.01.2026",
   "newCalls": [
     {
-      "name": "Bu hafta yeni açılan çağrı bulunmamaktadır.",
-      "code": "",
-      "openDate": "",
-      "closeDate": "",
-      "budget": "",
-      "link": "#"
+      "name": "Testing and optimising models of co-creation of advanced research infrastructure technologies",
+      "code": "HORIZON-INFRA-2027-TECH-01-01",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "10000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-tech-01-01"
+    },
+    {
+      "name": "Strengthening the potential of the EOSC for knowledge valorisation and industry-academia collaboration (EOSC Partnership)",
+      "code": "HORIZON-INFRA-2027-01-EOSC-02",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "2500000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-01-eosc-02"
+    },
+    {
+      "name": "Preparatory phase of new ESFRI research infrastructure projects",
+      "code": "HORIZON-INFRA-2027-DEV-01-01",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "1500000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-dev-01-01"
+    },
+    {
+      "name": "Expanding and deepening the EOSC Federation (EOSC Partnership)",
+      "code": "HORIZON-INFRA-2027-01-EOSC-01",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "40000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-01-eosc-01"
+    },
+    {
+      "name": "Access to research infrastructure services to enable R&I addressing EU priorities and emerging challenges",
+      "code": "HORIZON-INFRA-2027-SERV-01-02",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "6000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-serv-01-02"
+    },
+    {
+      "name": "Access to research infrastructures, their resources and services: large-scale pilots for more integrated scheme across (sub)domains",
+      "code": "HORIZON-INFRA-2027-SERV-01-01",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "35000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-serv-01-01"
+    },
+    {
+      "name": "Connecting research infrastructures and a wider user community across the European Research Area through access to advanced research infrastructure services",
+      "code": "HORIZON-INFRA-2027-SERV-01-03",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "3000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-serv-01-03"
+    },
+    {
+      "name": "Pioneering Destination Earth for a Sustainable Future: Large-Scale Pilots and Demonstrators",
+      "code": "HORIZON-INFRA-2027-TECH-01-02",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "7000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-tech-01-02"
+    },
+    {
+      "name": "Consolidation of the research infrastructure landscape – individual support for evolution, long-term sustainability and emerging needs of pan-European research infrastructures",
+      "code": "HORIZON-INFRA-2027-DEV-01-03",
+      "openDate": "2027-03-09",
+      "closeDate": "2027-06-15 00:00:00",
+      "budget": "3000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-infra-2027-dev-01-03"
+    },
+    {
+      "name": "Pillar I: Facilitating development of institutional open access policies through the retention of intellectual property rights",
+      "code": "HORIZON-WIDERA-2027-05-ERA-01",
+      "openDate": "2026-12-08",
+      "closeDate": "2027-03-11 00:00:00",
+      "budget": "2000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-widera-2027-05-era-01"
+    },
+    {
+      "name": "Pillar IV: Upgrading the EU’s independent knowledge on China's Science, Technology and Innovation system",
+      "code": "HORIZON-WIDERA-2027-05-ERA-05",
+      "openDate": "2026-12-08",
+      "closeDate": "2027-03-11 00:00:00",
+      "budget": "3000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-widera-2027-05-era-05"
+    },
+    {
+      "name": "Pillar III: Science comes to town 2029",
+      "code": "HORIZON-WIDERA-2027-05-ERA-03",
+      "openDate": "2026-12-08",
+      "closeDate": "2027-03-11 00:00:00",
+      "budget": "6000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-widera-2027-05-era-03"
+    },
+    {
+      "name": "Pillar III: Promoting public engagement in R&I and scientific literacy",
+      "code": "HORIZON-WIDERA-2027-05-ERA-04",
+      "openDate": "2026-12-08",
+      "closeDate": "2027-03-11 00:00:00",
+      "budget": "1000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-widera-2027-05-era-04"
+    },
+    {
+      "name": "Pillar II: Talent ecosystems for attractive early research careers",
+      "code": "HORIZON-WIDERA-2027-05-ERA-02",
+      "openDate": "2026-12-08",
+      "closeDate": "2027-03-11 00:00:00",
+      "budget": "2000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-widera-2027-05-era-02"
+    },
+    {
+      "name": "RAPTOR 2026 Open Call",
+      "code": "HORIZON-EIT-2025-KIC-IBA-UM",
+      "openDate": "2026-01-16",
+      "closeDate": "2026-03-12 00:00:00",
+      "budget": "900000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-eit-2025-kic-iba-um"
+    },
+    {
+      "name": "Boosting innovation through better integration of fragmented health R&I efforts",
+      "code": "HORIZON-JU-IHI-2026-12-SINGLE-STAGE-02",
+      "openDate": "2026-01-15",
+      "closeDate": "2026-04-21 00:00:00",
+      "budget": "15000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-ihi-2026-12-single-stage-02"
+    },
+    {
+      "name": "Boosting innovation for a better understanding of the determinants of health",
+      "code": "HORIZON-JU-IHI-2026-12-SINGLE-STAGE-01",
+      "openDate": "2026-01-15",
+      "closeDate": "2026-04-21 00:00:00",
+      "budget": "8000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-ihi-2026-12-single-stage-01"
+    },
+    {
+      "name": "Boosting innovation for people-centred integrated healthcare solutions",
+      "code": "HORIZON-JU-IHI-2026-12-SINGLE-STAGE-03",
+      "openDate": "2026-01-15",
+      "closeDate": "2026-04-21 00:00:00",
+      "budget": "8000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-ihi-2026-12-single-stage-03"
+    },
+    {
+      "name": "Boosting innovation through exploitation of digitalisation and data exchange in healthcare",
+      "code": "HORIZON-JU-IHI-2026-12-SINGLE-STAGE-04",
+      "openDate": "2026-01-15",
+      "closeDate": "2026-04-21 00:00:00",
+      "budget": "8000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-ihi-2026-12-single-stage-04"
+    },
+    {
+      "name": "Boosting innovation for better assessment of the added value of innovative integrated healthcare solutions",
+      "code": "HORIZON-JU-IHI-2026-12-SINGLE-STAGE-05",
+      "openDate": "2026-01-15",
+      "closeDate": "2026-04-21 00:00:00",
+      "budget": "5000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-ihi-2026-12-single-stage-05"
+    },
+    {
+      "name": "Global collaboration action for prevention and treatment of Lower Respiratory Tract Infections (LRTIs) in sub-Saharan Africa",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-01-LRTI-02-two-stage",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-17 00:00:00",
+      "budget": "8475000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-01-lrti-02-two-stage"
+    },
+    {
+      "name": "Global collaboration action towards better prevention, treatment and clinical management of HIV co-infections or co-morbidities in sub-Saharan Africa",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-01-HIV-03-two-stage",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-17 00:00:00",
+      "budget": "5000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-01-hiv-03-two-stage"
+    },
+    {
+      "name": "Global collaboration action on climate and health in sub-Saharan Africa",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-02-CH-01-two-stage",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-17 00:00:00",
+      "budget": "5000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-02-ch-01-two-stage"
+    },
+    {
+      "name": "Training and innovation networks for sustained capacity development related to ethics, regulatory, pharmacovigilance, and related digital regulatory platforms",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-03-SERP-01",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-02 00:00:00",
+      "budget": "1500000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-03-serp-01"
+    },
+    {
+      "name": "Enhancing integrated research and healthcare in sub-Saharan Africa through digital innovation and Artificial Intelligence",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-03-DIGIT-02",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-02 00:00:00",
+      "budget": "2250000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-03-digit-02"
+    },
+    {
+      "name": "Global collaboration action for the development of TB drugs for therapy and chemoprophylaxis in adults and children in sub-Saharan Africa",
+      "code": "HORIZON-JU-GH-EDCTP3-2026-01-TB-01-two-stage",
+      "openDate": "2026-01-14",
+      "closeDate": "2026-09-17 00:00:00",
+      "budget": "10000000",
+      "link": "https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-ju-gh-edctp3-2026-01-tb-01-two-stage"
     }
   ],
   "missionCalls": [
