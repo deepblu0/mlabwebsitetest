6dc3de7bfd45af7671b1142f4d4a9637c7a429f4
deepblu0 <berkay.yilmaz.edu@gmail.com>
2026-02-06 10:13:10 +0300

    Fix HTML rendering in translations (use innerHTML)

--- i18n.js
+++ i18n.js
@@ -1228,7 +1228,7 @@ function applyTranslations() {
     // Translate text content
     document.querySelectorAll('[data-i18n]').forEach(element => {
         const key = element.getAttribute('data-i18n');
-        element.textContent = t(key);
+        element.innerHTML = t(key);
     });
 
     // Translate placeholders
